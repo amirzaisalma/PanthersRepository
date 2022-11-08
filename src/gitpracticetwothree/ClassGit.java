4d073f364db0769aae2a1403da03516577789e52
panthersclass <eisa_amirzai@yahoo.com>
2022-11-08 13:24:42 -0500

    this is git

---
+++ src/gitpracticetwothree/ClassGit.java
@@ -0,0 +1,11 @@
+package gitpracticetwothree;
+
+public class ClassGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is git ");
+System.out.println("emty git");
+	}
+
+}
